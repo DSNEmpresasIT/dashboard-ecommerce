ed1114b4c259570f2942ae0d45b8fda487669fb5
DanielMino19 <danielmino926@gmail.com>
2023-12-14 13:48:39 -0300

    update img in cloudinary

--- src/app/components/form-product/form-product.component.ts
+++ src/app/components/form-product/form-product.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../interfaces/product';
 import { SupabaseService } from '../../services/supabase/supabase.service';
 import { ButtonSpinerComponent } from "../button-spiner/button-spiner.component";
 import { AlertService, AlertsType } from '../../services/alert.service';
+import { CloudinaryService } from '../../services/cloudinary.service';
 
 @Component({
     selector: 'app-form-product',
@@ -19,8 +20,9 @@ export class FormProductComponent implements OnInit {
   renderProduct: Product | undefined;
   @Output() booleanOutput: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private formBuilder: FormBuilder, private supaBase : SupabaseService,
-    private alertServ: AlertService
+  constructor(private formBuilder: FormBuilder, private supabase : SupabaseService,
+    private alertServ: AlertService,
+    private cloudinaryService: CloudinaryService
   ) {
     this.productForm = this.formBuilder.group({
       id: [this.renderProduct?.id],
@@ -40,7 +42,7 @@ export class FormProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.supaBase.editProduct.subscribe((editedProduct: Product | null) => {
+    this.supabase.editProduct.subscribe((editedProduct: Product | null) => {
       if (editedProduct) {
         this.renderProduct = editedProduct;
         this.updateFormValues();
@@ -55,15 +57,20 @@ export class FormProductComponent implements OnInit {
       const reader = new FileReader();
   
       const maxSizeInBytes = 500 * 1024; // 500kb
-
+  
       if (file.size > maxSizeInBytes) {
         console.error('The file exceeds the maximum allowed size.');
-        this.alertServ.show(6000, "La imagen supera el tamaño maximo de 500kb", AlertsType.ERROR)
+        this.alertServ.show(6000, "La imagen supera el tamaño maximo de 500kb", AlertsType.ERROR);
         return;
       }
-
+  
       await new Promise<void>((resolve, reject) => {
-        reader.onload = (e: any) => {
+        reader.onload = async (e: any) => {
+     
+          if (this.renderProduct?.img) {
+            // Elimina la imagen antigua de Cloudinary antes de asignar la nueva
+            await this.cloudinaryService.onEditImageSelected(this.renderProduct.img);
+          }
           this.productForm.get('img')?.setValue(e.target.result);
           resolve();
         };
@@ -78,22 +85,83 @@ export class FormProductComponent implements OnInit {
   }
   
 
-  onSubmit() {
+  async onSubmit(): Promise<void> {
     if (this.productForm.valid) {
-      const formData: Product = this.productForm.value;
-      console.log(formData.id);
-      this.toggleLoading()
-      this.supaBase.updateProduct(formData)
-        .then((result) => {
-          this.toggleModal() // cerrar el modal del form
-          this.supaBase.updateProducts()
-          this.alertServ.show(6000, "Producto actualizado con exito", AlertsType.SUCCESS);
-        })
-        .catch((error) => {
-          this.toggleLoading()
-          console.error('Error al actualizar el producto:', error);
-          this.alertServ.show(6000, "Error al actualizar el producto", AlertsType.ERROR);
-        });
+      this.toggleLoading();
+  
+      try {
+        const cloudinaryUrl = await this.uploadImageToCloudinary();
+        if (cloudinaryUrl) {
+          const productData = this.prepareProductData(cloudinaryUrl);
+          await this.saveProductToSupabase(productData);
+        } else {
+          console.error('Cloudinary URL not available.');
+        }
+      } catch (error) {
+        console.error('Error processing the form:', error);
+      } finally {
+        this.toggleLoading();
+        this.toggleModal();
+      }
+    }
+  }
+
+  uploadToCloudinary(file: any): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.cloudinaryService.uploadImage(file).subscribe(
+        (response) => {
+          console.log('Image uploaded to Cloudinary:', response);
+          resolve(response);
+        },
+        (error) => {
+          console.error('Error uploading image to Cloudinary:', error);
+          reject(error);
+        }
+      );
+    });
+  }
+  
+  private async uploadImageToCloudinary(): Promise<string | undefined> {
+    try {
+      const imgBase64 = this.productForm.get('img')?.value;
+      if (imgBase64) {
+        if (this.renderProduct?.img) {
+          // Elimina la imagen antigua de Cloudinary antes de cargar la nueva
+          await this.cloudinaryService.onEditImageSelected(this.renderProduct.img);
+        }
+        const cloudinaryResponse = await this.uploadToCloudinary(imgBase64);
+        return cloudinaryResponse?.url;
+      } else {
+        console.error('The image is not available.');
+        return undefined;
+      }
+    } catch (error) {
+      console.error('Error uploading image to Cloudinary:', error);
+      return undefined;
+    }
+  }
+  
+  private prepareProductData(cloudinaryUrl: string): any {
+    const productData = { ...this.productForm.value };
+    productData.img = cloudinaryUrl;
+  
+    if ('selectedCategory' in productData) {
+      delete productData.selectedCategory;
+    }
+  
+    return productData;
+  }
+  
+  private async saveProductToSupabase(productData: any): Promise<void> {
+    try {
+      const newProduct = await this.supabase.updateProduct(productData);
+      if (newProduct) {
+        this.alertServ.show(6000, "producto agregado con exito", AlertsType.SUCCESS)
+        this.supabase.updateProducts();
+      }
+    } catch (error) {
+      this.alertServ.show(6000, "Error al agregar el producto", AlertsType.ERROR)
+
     }
   }
 
