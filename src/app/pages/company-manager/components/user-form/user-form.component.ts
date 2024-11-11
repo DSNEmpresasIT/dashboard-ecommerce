98e28b6b8cf12595f42be0ce52a66512a36d3dca
iarmolla <ivan.armolla@gmail.com>
2024-11-11 20:09:26 -0300

    feat: add functionality to update user

--- src/app/pages/company-manager/components/user-form/user-form.component.ts
+++ src/app/pages/company-manager/components/user-form/user-form.component.ts
@@ -17,58 +17,56 @@ import { ModalButtonComponent } from "../../../../components/modal-button/modal-
 import { CrudAction } from '../../../../enums/enums';
 import { UserService } from '../../../../services/global-api/company-manager/user.service';
 import { firstValueFrom } from 'rxjs';
+import { AlertService, AlertsType } from '../../../../services/alert.service';
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule, MatDialogModule, MatButtonToggleModule, ReactiveFormsModule, ModalButtonComponent],
+  imports: [MatButtonModule, MatDialogModule, MatButtonToggleModule, ReactiveFormsModule, ModalButtonComponent],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
   userService = inject(UserService)
+  alertService = inject(AlertService)
   crud = CrudAction
   title!: string
-  errorMsg!: boolean
   userForm: FormGroup = new FormGroup({
     id: new FormControl(''),
     email: new FormControl('', [Validators.required, Validators.email]),
     userName: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.minLength(10)])
+    password: new FormControl('', [Validators.minLength(4)])
   });
   constructor(@Inject(MAT_DIALOG_DATA) public data: Data, private dialogRef: MatDialogRef<UserFormComponent>) {
-    console.log(data);
     this.userForm.patchValue({
-      id: data.user?.id,
-      email: data?.user?.email,
-      userName: data?.user?.user_name
+      id: data.user?.id || '',
+      email: data?.user?.email || '',
+      userName: data?.user?.user_name || ''
     })
     this.title = this.data.action
+    if (data.action === CrudAction.CREATE) {
+      this.userForm.get('password')?.setValidators([Validators.required, Validators.minLength(4)]);
+      this.userForm.get('password')?.updateValueAndValidity();
+    }
     if (data.action === CrudAction.READ) {
       this.userForm.disable()
     }
   }
   async submit() {
-    // this.dialogRef.close()
-    if(!this.userForm.valid) return
+    if (!this.userForm.valid) return
     try {
       switch (this.data.action) {
         case CrudAction.CREATE:
-          
+
           break;
-  
-        case CrudAction.READ:
-          break;
-  
-        case CrudAction.DELETE:
-          break;
-  
         case CrudAction.UPDATE:
-            const response = await firstValueFrom(this.userService.updateUser(this.userForm.get('id')?.value, this.userForm.value))
-            console.log(response);
+          await firstValueFrom(this.userService.updateUser(this.userForm.get('id')?.value, this.userForm.value))
+          await this.data.refresh()
+          this.dialogRef.close()
           break;
       }
     } catch (error) {
       console.error(error)
+      this.alertService.show(4000, "Se produjo un error al intentar actualizar la información del usuario", AlertsType.ERROR);
     }
   }
 }
